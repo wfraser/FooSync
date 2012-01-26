fbd42defa2d360f305072790b6941ffa2ebfd08d
William R. Fraser <wfraser@codewise.org>
2012-01-25 17:40:29 -0800

    ...

--- FooSync.ConsoleApp/Program.cs
+++ FooSync.ConsoleApp/Program.cs
@@ -33,6 +33,10 @@ namespace FooSync.ConsoleApp
                 Console.WriteLine("Using the Mono runtime.");
             }
 
+            //
+            // Load the repository config
+            //
+
             string repoConfigError;
             RepositoryConfig config = RepositoryConfigLoader.GetRepositoryConfig(FooSync.ConfigFileName, out repoConfigError);
 
@@ -55,6 +59,10 @@ namespace FooSync.ConsoleApp
 
                 var exceptions = FooSync.PrepareExceptions(dir);
 
+                //
+                // Enumerate files in the source and repository trees
+                //
+
                 FooTree repo = GetFooTree(dir.Path, exceptions, "repository");
                 if (repo == null)
                     return;
@@ -63,38 +71,45 @@ namespace FooSync.ConsoleApp
                 if (source == null)
                     return;
 
+                //
+                // Compute & display the change set
+                //
+
                 var changedFiles = Foo.Inspect(repo, source);
 
                 Console.WriteLine("File changes:");
                 foreach (var file in changedFiles)
                 {
-                    string descr = "ERROR";
-                    switch (file.Value.Status)
+                    string descr = string.Empty;
+                    switch (file.Value.ChangeStatus)
                     {
-                        case FooFileInfo.ChangeStatus.Identical:
-                        case FooFileInfo.ChangeStatus.Undetermined:
-                            Debug.Assert(false, "Bogus file change state");
-                            break;
+                        case ChangeStatus.Identical:
+                        case ChangeStatus.Undetermined:
+                            throw new ApplicationException("Bogus file change state");
 
-                        case FooFileInfo.ChangeStatus.Newer:
+                        case ChangeStatus.Newer:
                             descr = "Newer than the repository";
                             break;
 
-                        case FooFileInfo.ChangeStatus.Older:
+                        case ChangeStatus.Older:
                             descr = "Older than the repository";
                             break;
 
-                        case FooFileInfo.ChangeStatus.RepoMissing:
+                        case ChangeStatus.RepoMissing:
                             descr = "Not in the repository";
                             break;
 
-                        case FooFileInfo.ChangeStatus.SourceMissing:
+                        case ChangeStatus.SourceMissing:
                             descr = "Not in our directory";
                             break;
                     }
 
                     Console.WriteLine("\t{0}: {1}", descr, file.Key);
                 }
+
+                //
+                // Load / generate the repository state
+                //
                 
                 RepositoryState state;
                 try
@@ -113,17 +128,172 @@ namespace FooSync.ConsoleApp
                     return;
                 }
 
+                bool stateChanged = false;
                 if (state.Repository == null)
                 {
-                    state.AddSource(source, RepositoryState.RepoSourceName);
+                    state.AddSource(repo, RepositoryState.RepoSourceName);
+                    stateChanged = true;
                 }
 
                 if (!state.Sources.ContainsKey(Environment.MachineName.ToLower()))
                 {
-                    state.AddSource(repo, Environment.MachineName.ToLower());
+                    state.AddSource(source, Environment.MachineName.ToLower());
+                    stateChanged = true;
                 }
 
-                state.Write(FooSync.RepoStateFileName);
+                if (stateChanged)
+                {
+                    state.Write(FooSync.RepoStateFileName);
+                }
+
+                //
+                // Check against the repository state
+                //
+
+                var conflicts = Foo.GetConflicts(changedFiles, state);
+                var copyOperations = new List<KeyValuePair<string, CopyOperation>>();
+
+                foreach (var change in changedFiles)
+                {
+                    if (change.Value.ConflictStatus == ConflictStatus.NoConflict)
+                    {
+                        switch (change.Value.ChangeStatus)
+                        {
+                            case ChangeStatus.Newer:
+                            case ChangeStatus.RepoMissing:
+                                copyOperations.Add(new KeyValuePair<string, CopyOperation>(change.Key, CopyOperation.UseSource));
+                                break;
+
+                            case ChangeStatus.Older:
+                            case ChangeStatus.SourceMissing:
+                                copyOperations.Add(new KeyValuePair<string, CopyOperation>(change.Key, CopyOperation.UseRepo));
+                                break;
+
+                            default:
+                                throw new ApplicationException("Error, invalid change status!");
+                        }
+                    }
+                    // else: no-op, these are handled by the conflicts loop below
+                }
+
+                if (conflicts.Count > 0) {
+                    Console.WriteLine("\n{0} Conflicts:\n", conflicts.Count);
+                }
+                foreach (var conflict in conflicts)
+                {
+                    string descr = string.Empty;
+                    switch (conflict.Value.ConflictStatus)
+                    {
+                        case ConflictStatus.DeletedInRepo:
+                            descr = "File changed in the source and deleted in the repository";
+                            break;
+
+                        case ConflictStatus.DeletedInSource:
+                            descr = "File changed in the repository and deleted in the source";
+                            break;
+
+                        case ConflictStatus.RepoChanged:
+                            descr = "File changed in the repository and in the source (source newer)";
+                            break;
+
+                        case ConflictStatus.SourceChanged:
+                            descr = "File changed in the repository and in the source (repository newer)";
+                            break;
+                    }
+
+                    Console.WriteLine("{0}:\n{1}", descr, conflict.Key);
+                    if (conflict.Value.ConflictStatus != ConflictStatus.DeletedInSource)
+                    {
+                        Console.WriteLine("\tSource:     modified {0}, {1} bytes",
+                            source.Files[conflict.Key].MTime.ToString(),
+                            source.Files[conflict.Key].Size);
+                    }
+                    if (conflict.Value.ConflictStatus != ConflictStatus.DeletedInRepo)
+                    {
+                        Console.WriteLine("\tRepository: modified {0}, {1} bytes",
+                            repo.Files[conflict.Key].MTime.ToString(),
+                            repo.Files[conflict.Key].Size);
+                    }
+
+                    KeyValuePair<string, CopyOperation>? action = GetActionFromUser(conflict.Key);
+                    if (action.HasValue)
+                    {
+                        copyOperations.Add(action.Value);
+                    }
+                }
+
+                //
+                // Display overview of actions to be taken
+                //
+
+                bool accepted = false;
+                while (!accepted)
+                {
+                    Console.WriteLine("\nActions to be taken:\n");
+
+                    int n = 1;
+                    foreach (var change in copyOperations)
+                    {
+                        string sfile = Path.GetFullPath(Path.Combine(source.Path, change.Key));
+                        string rfile = Path.GetFullPath(Path.Combine(repo.Path, change.Key));
+                        string num = string.Format("{0,"
+                            + Math.Ceiling(Math.Log10(copyOperations.Count)).ToString()
+                            + "}",
+                            n++);
+
+                        bool useRepo = (change.Value == CopyOperation.UseRepo);
+
+                        Console.WriteLine("{0}: {1}\n    -> {2}",
+                            num,
+                            useRepo ? rfile : sfile,
+                            useRepo ? sfile : rfile);
+                    }
+
+                    Console.Write("\nPress Enter to perform actions, or enter a number to edit one: ");
+                    string changeNum = Console.ReadLine();
+
+                    if (changeNum == string.Empty)
+                    {
+                        accepted = true;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            n = int.Parse(changeNum) - 1;
+                            if (n < 0 || n >= copyOperations.Count)
+                                throw new FormatException("out of range");
+
+                            string path = copyOperations[n].Key;
+
+                            var action = GetActionFromUser(path);
+                            if (action.HasValue)
+                            {
+                                copyOperations[n] = action.Value;
+                            }
+                            else
+                            {
+                                copyOperations.RemoveAt(n);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex is FormatException
+                                || ex is OverflowException)
+                            {
+                                Console.WriteLine("invalid selection");
+                            }
+                            else
+                            {
+                                throw ex;
+                            }
+                        }
+                    }
+                }
+
+                //
+                // Perform the operations
+                //
             }
         }
 
@@ -152,15 +322,46 @@ namespace FooSync.ConsoleApp
             return ret;
         }
 
+        private KeyValuePair<string, CopyOperation>? GetActionFromUser(string path)
+        {
+            KeyValuePair<string, CopyOperation>? ret = null;
+
+            bool actionAccepted = false;
+            while (!actionAccepted)
+            {
+                Console.Write("Action: Take (R)epository | Take (S)ource | Do (N)othing : ");
+                var key = Console.ReadKey(false);
+                actionAccepted = true;
+                switch (key.Key)
+                {
+                    case ConsoleKey.R:
+                        ret = new KeyValuePair<string, CopyOperation>(path, CopyOperation.UseRepo);
+                        break;
+
+                    case ConsoleKey.S:
+                        ret = new KeyValuePair<string, CopyOperation>(path, CopyOperation.UseSource);
+                        break;
+
+                    case ConsoleKey.N:
+                        ret = null;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid key pressed.");
+                        actionAccepted = false;
+                        break;
+                }
+            }
+
+            return ret;
+        }
+
         private FooSync Foo { get; set; }
 
-        enum ChangeStatus
+        private enum CopyOperation
         {
-            Identical = 0,
-            Older = 1,
-            Newer = -1,
-            RepoMissing = 2,
-            SourceMissing = 3
+            UseRepo,
+            UseSource
         }
     }
 }
