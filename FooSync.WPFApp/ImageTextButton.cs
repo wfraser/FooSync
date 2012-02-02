ca97899ce53eb4b67835321b64b962c8c045f517
William R. Fraser <wfraser@codewise.org>
2012-02-01 18:35:41 -0800

    beginnings of the WPF GUI

---
+++ FooSync.WPFApp/ImageTextButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FooSync.WPFApp
+{
+    /// <summary>
+    /// A button with an image and text below it.
+    /// </summary>
+    public class ImageTextButton : Button
+    {
+        static ImageTextButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageTextButton), new FrameworkPropertyMetadata(typeof(ImageTextButton)));
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageTextButton), new PropertyMetadata(null));
+    }
+}
