614c6800585602842c0cdfd832f3734bd2c4ef0f
William R. Fraser <wfraser@codewise.org>
2012-03-29 21:49:55 -0700

    add a simpler tab to the create repository window

--- FooSync.WPFApp/CreateRepositoryWindow.xaml.cs
+++ FooSync.WPFApp/CreateRepositoryWindow.xaml.cs
@@ -37,12 +37,13 @@ namespace Codewise.FooSync.WPFApp
             Close();
         }
 
-        private void Create_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Validate all paths in the TextBox elements of the selected setup tab. Highlites invalid
+        /// paths.
+        /// </summary>
+        /// <returns>true if no errors, false otherwise.</returns>
+        private bool ValidatePaths()
         {
-            //
-            // Validate controls
-            //
-
             var errorElements = new List<Control>();
 
             if (string.IsNullOrEmpty(RepositoryName.Text))
@@ -50,24 +51,29 @@ namespace Codewise.FooSync.WPFApp
                 errorElements.Add(RepositoryName);
             }
 
-            if (string.IsNullOrEmpty(RepositoryLocation.Text) || !Directory.Exists(RepositoryLocation.Text))
+            if (SetupTabs.SelectedItem == SimpleSetupTab)
             {
-                errorElements.Add(RepositoryLocation);
-            }
+                if (string.IsNullOrEmpty(SimpleRepositoryPath.Text) || !Directory.Exists(SimpleRepositoryPath.Text))
+                    errorElements.Add(SimpleRepositoryPath);
 
-            if (string.IsNullOrEmpty(SubdirName.Text) || SubdirName.Text.IndexOfAny(Path.GetInvalidPathChars()) != -1)
-            {
-                errorElements.Add(SubdirName);
+                if (string.IsNullOrEmpty(SimpleSourcePath.Text) || !Directory.Exists(SimpleSourcePath.Text))
+                    errorElements.Add(SimpleSourcePath);
             }
-
-            if (string.IsNullOrEmpty(SubdirSourcePath.Text) || !Directory.Exists(SubdirSourcePath.Text))
+            else if (SetupTabs.SelectedItem == AdvancedSetupTab)
             {
-                errorElements.Add(SubdirSourcePath);
+                if (string.IsNullOrEmpty(RepositoryLocation.Text) || !Directory.Exists(RepositoryLocation.Text))
+                    errorElements.Add(RepositoryLocation);
+
+                if (string.IsNullOrEmpty(SubdirName.Text) || SubdirName.Text.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+                    errorElements.Add(SubdirName);
+
+                if (string.IsNullOrEmpty(SubdirSourcePath.Text) || !Directory.Exists(SubdirSourcePath.Text))
+                    errorElements.Add(SubdirSourcePath);
             }
 
             if (errorElements.Count > 0)
             {
-                ValidationErrors.Content = string.Format("{0} required fields empty or invalid", errorElements.Count);
+                ValidationErrors.Content = string.Format("{0} required field{1} empty or invalid", errorElements.Count, (errorElements.Count == 1) ? string.Empty : "s");
 
                 foreach (var elem in errorElements)
                 {
@@ -83,11 +89,75 @@ namespace Codewise.FooSync.WPFApp
                         ((ComboBox)elem).PreviewTextInput += new System.Windows.Input.TextCompositionEventHandler(RemoveBorderBrush);
                     }
                 }
-                return;
+                return false;
             }
 
             ValidationErrors.Content = "";
 
+            return true;
+        }
+
+        /// <summary>
+        /// Handles clicks on the Create Repository button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidatePaths())
+                return;
+
+            if (SetupTabs.SelectedItem == SimpleSetupTab)
+            {
+                DialogResult = SimpleSetup();
+            }
+            else if (SetupTabs.SelectedItem == AdvancedSetupTab)
+            {
+                DialogResult = AdvancedSetup();
+            }
+
+            Close();
+        }
+
+        /// <summary>
+        /// Handles creating and writing a new RepositoryConfig when the user has used the Simple Setup tab.
+        /// </summary>
+        /// <returns>true if successful.</returns>
+        private bool SimpleSetup()
+        {
+            RepositoryPath = SimpleRepositoryPath.Text;
+
+            var config = new RepositoryConfig();
+            config.RepositoryName = RepositoryName.Text;
+            config.Filename = Path.Combine(RepositoryPath, FooSyncEngine.ConfigFileName);
+            config.Directories = new RepositoryDirectory[]
+            {
+                new RepositoryDirectory()
+                {
+                    Path = ".",
+
+                    Sources = new RepositorySource[]
+                    {
+                        new RepositorySource()
+                        {
+                            Name = Environment.MachineName.ToLower(),
+                            Path = SimpleSourcePath.Text
+                        }
+                    }
+                }
+            };
+
+            RepositoryConfigLoader.WriteRepositoryConfig(config, config.Filename);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Handles creating and writing a new RepositoryConfig when the user has used the Advanced Setup tab.
+        /// </summary>
+        /// <returns>true if successful.</returns>
+        private bool AdvancedSetup()
+        {
             //
             // Create new RepositoryConfig
             //
@@ -123,9 +193,7 @@ namespace Codewise.FooSync.WPFApp
 
             RepositoryConfigLoader.WriteRepositoryConfig(config, config.Filename);
 
-            DialogResult = true;
-
-            Close();
+            return true;
         }
 
         private void RemoveBorderBrush(object sender, RoutedEventArgs e)
@@ -149,6 +217,13 @@ namespace Codewise.FooSync.WPFApp
             }
         }
 
+        /// <summary>
+        /// Handles a click on a Browse button.
+        /// The Button that sends this should have a Tag attribute populated with a reference to
+        /// the TextBox which is to receive the result of the browsing.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FolderBrowse_Click(object sender, RoutedEventArgs e)
         {
             if (sender is FrameworkElement)
@@ -170,6 +245,11 @@ namespace Codewise.FooSync.WPFApp
             }
         }
 
+        /// <summary>
+        /// Presents the user with a folder browser dialog.
+        /// </summary>
+        /// <param name="filter">Filename filter for if the OS doesn't support a folder browser.</param>
+        /// <returns>Full path the user selected.</returns>
         private string ShowFolderBrowser(string filter)
         {
             bool cancelled = false;
@@ -203,22 +283,56 @@ namespace Codewise.FooSync.WPFApp
             return path;
         }
 
-        private void RepositoryLocation_TextChanged(object sender, TextChangedEventArgs e)
+        /// <summary>
+        /// Invoked when user changes the Repository Location.
+        /// If the Repository Name field is empty, it is set to the repository directory name.
+        /// 
+        /// If the Advanced Setup tab is open, this also populates the list of subdirectories in
+        /// the Subdirectory Name combobox.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RepositoryLocation_TextChanged(object sender, RoutedEventArgs e)
         {
-            var dir = ((TextBox)sender).Text;
-            if (Directory.Exists(dir))
+            // don't update until after the textbox has lost focus
+            if (((TextBox)sender).IsFocused)
             {
-                SubdirName.DataContext = (from subdir in Directory.EnumerateDirectories(dir, "*", SearchOption.TopDirectoryOnly)
-                                          select Path.GetFileName(subdir));
+                if (!_lostFocusTextBoxes.Contains((TextBox)sender))
+                {
+                    ((TextBox)sender).LostFocus += RepositoryLocation_TextChanged;
+                    _lostFocusTextBoxes.Add((TextBox)sender);
+                }
+                return;
             }
-            else
+            else if (_lostFocusTextBoxes.Contains((TextBox)sender))
             {
-                SubdirName.DataContext = null;
+                ((TextBox)sender).LostFocus -= RepositoryLocation_TextChanged;
+            }
+
+            var dir = ((TextBox)sender).Text;
+
+            if (string.IsNullOrEmpty(RepositoryName.Text))
+            {
+                RepositoryName.Text = Path.GetFileName(dir);
+            }
+            
+            if (SetupTabs.SelectedItem == AdvancedSetupTab)
+            {
+                if (Directory.Exists(dir))
+                {
+                    SubdirName.DataContext = (from subdir in Directory.EnumerateDirectories(dir, "*", SearchOption.TopDirectoryOnly)
+                                              select Path.GetFileName(subdir));
+                }
+                else
+                {
+                    SubdirName.DataContext = null;
+                }
             }
         }
 
         public string RepositoryPath { get; set; }
 
         private Brush _defaultTextBoxBorderBrush;
+        private HashSet<TextBox> _lostFocusTextBoxes = new HashSet<TextBox>();
     }
 }
