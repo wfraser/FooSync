ae3734cd877297025387e52e2fbdef508bcdf3db
William R. Fraser <wfraser@codewise.org>
2012-04-26 03:13:12 -0700

    Starting the rewrite of the WPF UI...

---
+++ FooSync.WPFApp2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Codewise.FooSync.WPFApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowAboutWindow(object sender, RoutedEventArgs e)
+        {
+            var about = new AboutWindow();
+            about.ShowActivated = true;
+            about.ShowDialog();
+        }
+
+        private void NewLocalPair(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void NewRemoteServer(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void NewRemotePair(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
