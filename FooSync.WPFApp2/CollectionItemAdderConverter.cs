8476bc4f6f5a4abe20e586dd2158b674c26c1b9e
William R. Fraser <wfraser@codewise.org>
2012-05-14 16:38:00 -0700

    doc

--- FooSync.WPFApp2/CollectionItemAdderConverter.cs
+++ FooSync.WPFApp2/CollectionItemAdderConverter.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿///
+/// Codewise/FooSync/WPFApp2/CollectionItemAdderConverter.cs
+/// 
+/// by William R. Fraser
+///     http://www.codewise.org/
+///     https://github.com/wfraser/FooSync
+///     
+/// Copyright (c) 2012
+/// 
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -9,8 +19,22 @@ using System.Windows.Data;
 
 namespace Codewise.FooSync.WPFApp2
 {
+    /// <summary>
+    /// "Converts" a collection by copying it and appending an item (the converter parameter) to it.
+    /// </summary>
     public class CollectionItemAdderConverter : IValueConverter
     {
+        
+        /// <summary>
+        /// "Converts" a collection by copying it and appending an item (the converter parameter)
+        /// to it.
+        /// </summary>
+        /// <param name="value">Must implement ICollection (not generic!)</param>
+        /// <param name="targetType">ignored</param>
+        /// <param name="parameter">Object to append to the collection. If it is a DataTemplate,
+        /// it is instantiated first.</param>
+        /// <param name="culture">ignored</param>
+        /// <returns>An ArrayList with the items in the input collection, plus the new one.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (parameter == null)
@@ -35,6 +59,9 @@ namespace Codewise.FooSync.WPFApp2
             return newList;
         }
 
+        /// <summary>
+        /// Not supported.
+        /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
